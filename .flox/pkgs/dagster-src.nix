5a5d6b0d1e8bfaa708391f80ae16aa770bab9dc2
steve <steve@flox.dev>
2025-11-06 09:33:39 -0500

    Add correct source hash for dagster-src

--- .flox/pkgs/dagster-src.nix
+++ .flox/pkgs/dagster-src.nix
@@ -7,5 +7,5 @@ fetchFromGitHub {
   repo = "dagster";
   rev = "master";  # Or specify a commit/tag like "1.8.7"
   # Leave empty hash initially - flox build will tell you the correct one
-  hash = "";
+  hash = "sha256-hR3QRm5ihtTcAO6zHSVKGNUiL+MEflC4Bm1YqQ+lvf4=";
 }
